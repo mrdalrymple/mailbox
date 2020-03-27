d88cf64c6eeae87bd249b141bab58a9f91ec05ae
Matthew Dalrymple <matt.dalrymple@gmail.com>
2020-03-27 00:52:16 -0700

    Add 'mb pull'
    Add the command 'mb pull' to execute the inbox phase by itself.

--- libmailcd/main.py
+++ libmailcd/main.py
@@ -85,6 +85,70 @@ def exec_stage(config, stage_name, stage):
 
 ########################################
 
+def workflow_inbox_run(workspace, pipeline_inbox):
+    env_vars = []
+
+    if pipeline_inbox:
+        inbox_packages = [] # all packages
+        packages_to_download = [] # all packages that need to be downloaded
+
+        # Find required packages
+        for slot in pipeline_inbox:
+            logging.debug(f"{slot}")
+
+            tag = pipeline_inbox[slot]['tag']
+            logging.debug(f"tag={tag}")
+
+            labels = tag
+            storage_id = slot
+
+            matches = libmailcd.storage.find(storage_id, labels)
+            if len(matches) > 1:
+                raise libmailcd.errors.StorageMultipleFound(storage_id, matches, f"multiple found in store '{storage_id}' with labels: {labels}")
+            if not matches:
+                raise ValueError(f"No matches found for '{storage_id}' with labels: {labels}")
+            package_hash = matches[0]
+
+            pkg = {
+                "id": storage_id,
+                "hash": package_hash
+            }
+
+            packages_to_download.append(pkg)
+            inbox_packages.append(pkg)
+
+        # Download all required packages
+        # TODO(matthew): Do we need to optimize this to only actually download ones we don't already have
+        for package in packages_to_download:
+            storage_id = package['id']
+            package_hash = package['hash']
+            print(f"Downloading package: {storage_id}/{package_hash}")
+            # need a current workspace (cwd)
+            # calculate target directory
+            target_relpath = Path(LOCAL_MB_ROOT, LOCAL_INBOX_DIRNAME, storage_id, package_hash)
+            target_path = Path(workspace, target_relpath)
+
+            # download to the target directory
+            libmailcd.storage.download(storage_id, package_hash, target_path)
+            print(f" --> '{target_relpath}'")
+
+        # set env vars
+        for package in inbox_packages:
+            env_var_name = f"MB_{storage_id}_ROOT"
+            env_var_value = str(target_path)
+            os.environ[env_var_name] = env_var_value
+            env_vars.append(env_var_name)
+            logging.debug(f"SET {env_var_name}={env_var_value}")
+
+            env_var_name = f"MB_{storage_id}_ROOT_RELPATH"
+            env_var_value = str(target_relpath)
+            os.environ[env_var_name] = env_var_value
+            env_vars.append(env_var_name)
+            logging.debug(f"SET {env_var_name}={env_var_value}")
+
+    return env_vars
+
+########################################
 
 # TODO(matthew): enable logging and set default levels here
 # TODO(matthew): see if there is a way to pass --debug and update logging
@@ -103,9 +167,24 @@ def cli(debug):
     app_init(settings)
     pass
 
+
+@cli.command("pull")
+@click.option("--workspace", default=".")
+def cli_pull(workspace):
+    arg_workspace = Path(workspace).resolve()
+    pipeline_filepath = Path(arg_workspace, INSOURCE_PIPELINE_FILENAME).resolve()
+
+    pipeline = libmailcd.utils.load_yaml(pipeline_filepath)
+
+    pipeline_inbox = None
+    if 'inbox' in pipeline:
+        pipeline_inbox = pipeline['inbox']
+
+    if pipeline_inbox:
+        workflow_inbox_run(workspace, pipeline_inbox)
+
+
 @cli.command("clean")
-#@click.argument("storage_id")
-#@click.argument("package", type=click.Path(exists=True))  # can be zip or directory
 @click.option("--workspace", default=".")
 def cli_clean(workspace):
     arg_workspace = Path(workspace).resolve()
@@ -399,68 +478,9 @@ def cli_build(project_dir):
         #               INBOX                 #
         #######################################
         if pipeline_inbox:
-            inbox_packages = [] # all packages
-            packages_to_download = [] # all packages that need to be downloaded
-
-            # Find required packages
-            for slot in pipeline_inbox:
-                logging.debug(f"{slot}")
-
-                tag = pipeline_inbox[slot]['tag']
-                logging.debug(f"tag={tag}")
-
-                labels = tag
-                storage_id = slot
-
-                matches = libmailcd.storage.find(storage_id, labels)
-                if len(matches) > 1:
-                    raise libmailcd.errors.StorageMultipleFound(storage_id, matches, f"multiple found in store '{storage_id}' with labels: {labels}")
-                if not matches:
-                    raise ValueError(f"No matches found for '{storage_id}' with labels: {labels}")
-                package_hash = matches[0]
-
-                pkg = {
-                    "id": storage_id,
-                    "hash": package_hash
-                }
-
-                packages_to_download.append(pkg)
-                inbox_packages.append(pkg)
-
-            # Download all required packages
-            # TODO(matthew): Do we need to optimize this to only actually download ones we don't already have
             print(f"========== INBOX ==========")
-            for package in packages_to_download:
-                storage_id = package['id']
-                package_hash = package['hash']
-                print(f"Downloading package: {storage_id}/{package_hash}")
-                # need a current workspace (cwd)
-                # calculate target directory
-                target_relpath = Path(LOCAL_MB_ROOT, LOCAL_INBOX_DIRNAME, storage_id, package_hash)
-                target_path = Path(arg_project_dir, target_relpath)
-
-                # download to the target directory
-                libmailcd.storage.download(storage_id, package_hash, target_path)
-                print(f" --> '{target_relpath}'")
-
-            print(f"========== ===== ==========")
-
-            # set env vars
-            for package in inbox_packages:
-                env_var_name = f"MB_{storage_id}_ROOT"
-                env_var_value = str(target_path)
-                os.environ[env_var_name] = env_var_value
-                env_vars.append(env_var_name)
-                logging.debug(f"SET {env_var_name}={env_var_value}")
-
-                env_var_name = f"MB_{storage_id}_ROOT_RELPATH"
-                env_var_value = str(target_relpath)
-                os.environ[env_var_name] = env_var_value
-                env_vars.append(env_var_name)
-                logging.debug(f"SET {env_var_name}={env_var_value}")
-
+            env_vars = workflow_inbox_run(arg_project_dir, pipeline_inbox)
             print(f"===========================")
-
         #######################################
 
 
